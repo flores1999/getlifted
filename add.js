5abec07e008a3f7966a5ad24115adbf5531c5147
Jeffery Flores <jefferyf20@gmail.com>
2023-04-19 07:07:46 -0700

    update04

---
+++ add.js
@@ -0,0 +1,16 @@
+'use strict'; 
+
+alert("welcome to a stroger life")
+
+const username = prompt("what is your name");
+console.log(username);
+
+const input = prompt("what lift can we help you learn more about");
+console.log(input);
+
+
+document.write("welcome! "+ username +"   We are here to help you on your journey. ");
+document.write("Lets see if we can help you lear more about "+ input + ".");
+
+alert ("remember to practice good form" );
+
